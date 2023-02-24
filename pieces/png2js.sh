90258cb59c33e42eb8cc202a45bd4e7e16d372ad
rpvelloso <rvelloso@gmail.com>
2023-02-24 19:28:34 -0300

    change rect to 20x20; testing new collision

---
+++ pieces/png2js.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for img in *.png; do
+    echo -n '"data:image/png;base64, ' > $img.b64
+    (cat $img | base64 -i -w 0) >> $img.b64
+    echo -n '"' >> $img.b64
+done
+
+echo 'const piecesFiles = [' > images.js
+for b64 in *.b64; do
+    if [ "${b64}" != "rect.png.b64" ] && [ "${b64}" != "bg10x20.png.b64" ]; then
+        cat $b64 >> images.js
+        echo "," >> images.js
+    fi
+done
+echo '];' >> images.js
+echo -n 'const bg_image = createImage(' >> images.js
+cat bg10x20.png.b64 >> images.js
+echo ');' >> images.js
+echo -n 'const rect = createImage(' >> images.js
+cat rect.png.b64 >> images.js
+echo ');' >> images.js
+
+rm *.b64
